c4214165c92ff8e8b3a9f83d9c4b9a48b49c2b82
Setoue <proshigeru@gmail.com>
2023-08-17 11:32:44 -0300

    feat(components): add component list

--- training-react-with-typescript/writing-with-typescript/src/App.tsx
+++ training-react-with-typescript/writing-with-typescript/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Forms from "./components/Forms";
+import List from "./components/List";
 
 function App() {
   return (
     <div className="App">
       <h1>Writing with TypeScript</h1>
       <Forms />
+      <List />
     </div>
   );
 }
---
+++ training-react-with-typescript/writing-with-typescript/src/components/List/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const List = () => {
+  const tasks = [
+    { name: "React", time: "02:00:00" },
+    { name: "Javascript", time: "01:00:00" },
+    { name: "Typescript", time: "01:00:00" },
+    { name: "HTML", time: "00:30:00" },
+    { name: "CSS", time: "00:30:00" },
+  ];
+
+  return (
+    <aside>
+      <h2>Study day</h2>
+      <ul>
+        {tasks &&
+          tasks.map((task, index) => (
+            <li key={index}>
+              <h3>{task.name}</h3>
+              <span>{task.time}</span>
+            </li>
+          ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default List;
