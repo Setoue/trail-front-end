7a23ba1c941f084e6ac4b8da49d7973438d334d3
Setoue <proshigeru@gmail.com>
2023-08-18 09:40:47 -0300

    test(components): testing state in component list

--- training-react-with-typescript/writing-with-typescript/src/components/List/index.tsx
+++ training-react-with-typescript/writing-with-typescript/src/components/List/index.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 import style from "./List.module.scss";
 import Item from "./Item";
 
 const List = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { name: "React", time: "02:00:00" },
     { name: "Javascript", time: "01:00:00" },
     { name: "Typescript", time: "01:00:00" },
     { name: "HTML", time: "00:30:00" },
     { name: "CSS", time: "00:30:00" },
-  ];
+  ]);
+
+  const handleAddTask = () => {
+    setTasks([...tasks, { name: "State", time: "05:00" }]);
+  };
 
   return (
     <aside className={style.taskList}>
-      <h2>Study day</h2>
+      <h2 onClick={handleAddTask}>Study day</h2>
       <ul>
         {tasks && tasks.map((task, index) => <Item key={index} {...task} />)}
       </ul>
