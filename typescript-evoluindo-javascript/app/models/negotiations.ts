9e3c6fa194a15c7fdcb9e383d77fb397143e247c
Setoue <proshigeru@gmail.com>
2023-08-22 14:40:25 -0300

    refactor: shorten the attributes class negotiation

--- typescript-evoluindo-javascript/app/models/negotiations.ts
+++ typescript-evoluindo-javascript/app/models/negotiations.ts
@@ -7,7 +7,7 @@ export class Negotiations {
     this.negotiations = [...this.negotiations, negotiation];
   }
 
-  listNegotiations(): Array<Negotiation> {
+  listNegotiations(): ReadonlyArray<Negotiation> {
     return this.negotiations;
   }
 }
