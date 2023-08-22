ba1ecd491f90fd29f24987f5113aa9488b78a592
Setoue <proshigeru@gmail.com>
2023-08-22 14:26:44 -0300

    refactor: intance negotiations created and functions addNegotiations and lisNegotiations called in controller them

--- typescript-evoluindo-javascript/app/controllers/negotiation-controller.ts
+++ typescript-evoluindo-javascript/app/controllers/negotiation-controller.ts
@@ -1,9 +1,11 @@
 import { Negotiation } from "../models/negotiation.js";
+import { Negotiations } from "../models/negotiations.js";
 
 export class NegotiationController {
   private inputDate: HTMLInputElement;
   private inputAmount: HTMLInputElement;
   private inputValue: HTMLInputElement;
+  private negotiations: Negotiations = new Negotiations();
 
   constructor() {
     this.inputDate = document.querySelector("#date");
@@ -14,7 +16,9 @@ export class NegotiationController {
   add(): void {
     const negotiation = this.createNegotiation();
 
-    console.log(negotiation);
+    this.negotiations.addNegotiation(negotiation);
+    console.log(this.negotiations.listNegotiations());
+
     this.clearForms();
   }
 
