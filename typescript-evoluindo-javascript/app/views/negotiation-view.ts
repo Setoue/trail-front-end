d9697d77ab9841056aa5a8e3119259f8ac5dc701
Setoue <proshigeru@gmail.com>
2023-08-23 09:26:23 -0300

    feat: view negotiations created

---
+++ typescript-evoluindo-javascript/app/views/negotiation-view.ts
@@ -0,0 +1,18 @@
+export class NegotiationsView {
+  template(): string {
+    return `
+    
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Value</th>
+                </r>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    `;
+  }
+}
