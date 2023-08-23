8f4a310e57c12e59afdb95e770e1e6bb28f3917c
Setoue <proshigeru@gmail.com>
2023-08-23 09:13:53 -0300

    refactor: defensive programming

--- typescript-evoluindo-javascript/dist/js/models/negotiation.js
+++ typescript-evoluindo-javascript/dist/js/models/negotiation.js
@@ -1,8 +1,12 @@
 export class Negotiation {
-    constructor(date, amount, value) {
-        this.date = date;
+    constructor(_date, amount, value) {
+        this._date = _date;
         this.amount = amount;
         this.value = value;
+    }
+    get data() {
+        const date = new Date(this._date.getTime());
+        return date;
     }
     get volume() {
         return this.amount * this.value;
