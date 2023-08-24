c44d6a1d38add0ea42fb4e8c70b23b48c02e4266
Shigeru <proshigeru@gmail.com>
2023-08-24 13:48:17 -0300

    footer created and stylized

---
+++ practing-react/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>Desenvolvido por Shigeru</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- practing-react/src/pages/Start/index.js
+++ practing-react/src/pages/Start/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./Start.module.css";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const Start = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default Start;
