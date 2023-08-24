35b10a8655f2dcb77deb002278c8e9a5610e5bb0
Shigeru <proshigeru@gmail.com>
2023-08-24 13:31:05 -0300

    style: style component header and linkHeader added

--- practing-react/src/components/Header/index.js
+++ practing-react/src/components/Header/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import logo from "./Logo-cinetag-branco 1.png";
+import logo from "./logo.png";
 import HeaderLink from "../HeaderLink";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Link to="./">
-        <img src={logo} alt="log"></img>
-        <nav>
-          <HeaderLink url="./">Home</HeaderLink>
-          <HeaderLink url="./Favorite">Favorite</HeaderLink>
-        </nav>
+        <img src={logo} alt="log" />
       </Link>
+
+      <nav>
+        <HeaderLink url="./">Home</HeaderLink>
+        <HeaderLink url="./Favorite">Favorite</HeaderLink>
+      </nav>
     </header>
   );
 };
