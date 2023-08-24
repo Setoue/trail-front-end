cf5d93a5ebf28ce1108d301cb34c38991e5563d4
Shigeru <proshigeru@gmail.com>
2023-08-24 14:16:40 -0300

    feat: banner created and img insert

--- practing-react/src/pages/Start/index.js
+++ practing-react/src/pages/Start/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./Start.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Banner from "../../components/Banner";
 
 const Start = () => {
   return (
     <>
       <Header />
+      <Banner img="home" />
       <Footer />
     </>
   );
