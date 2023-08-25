b3dedc3f2df13c32ae40107cc1e00c3d60578799
Shigeru <proshigeru@gmail.com>
2023-08-25 13:41:49 -0300

    feat: page favorite created

--- practing-react/src/routes.js
+++ practing-react/src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
+import Favorite from "./pages/Favorite";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/favorite" element={<Favorite />} />
       </Routes>
     </BrowserRouter>
   );
