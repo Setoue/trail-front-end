fdc9e3bc2be1b3d87d37ea957abb498009dc6658
Shigeru <proshigeru@gmail.com>
2023-08-28 10:23:26 -0300

    feat: notfound implemented in routes

--- practing-react/src/routes.js
+++ practing-react/src/routes.js
@@ -6,6 +6,8 @@ import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Containuer from "./components/Containuer";
 import FavoriteProvider from "./context/Favorite.js";
+import Player from "./pages/Player";
+import NotFound from "./pages/NotFound";
 
 const AppRoutes = () => {
   return (
@@ -16,6 +18,8 @@ const AppRoutes = () => {
           <Routes>
             <Route path="/" element={<Start />} />
             <Route path="/favorite" element={<Favorite />} />
+            <Route path="/:id" element={<Player />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </FavoriteProvider>
       </Containuer>
