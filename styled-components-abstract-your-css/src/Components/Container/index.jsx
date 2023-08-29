a4891c3e4c01c3f4f3ce60bf35293f079630c044
Shigeru <proshigeru@gmail.com>
2023-08-29 15:06:41 -0300

    style: added media query

--- styled-components-abstract-your-css/src/Components/Container/index.jsx
+++ styled-components-abstract-your-css/src/Components/Container/index.jsx
@@ -7,6 +7,10 @@ const Container = styled.div`
   background-color: #f1f1f1;
   min-height: 90vh;
   padding: 0px 15vw;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 const Content = styled.section`
