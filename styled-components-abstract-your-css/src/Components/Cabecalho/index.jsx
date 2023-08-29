73e7ee1e488e4f19a97ebb3ef80baecebbf573a8
Shigeru <proshigeru@gmail.com>
2023-08-29 14:07:39 -0300

    refactor: created variables color and refactor tags

--- styled-components-abstract-your-css/src/Components/Cabecalho/index.jsx
+++ styled-components-abstract-your-css/src/Components/Cabecalho/index.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { colorFirst } from "../UI/variaveis.js";
 
+const StyledHeader = styled.nav`
+  background-color: ${colorFirst}
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const ImageLogo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
+
+const ButtonSecond = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: white;
+  color: #41d3be;
+`;
+
+const ButtonFirst = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  color: white;
+  background: transparent;
+`;
 const Cabecalho = () => {
   return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledHeader>
+      <ImageLogo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
-          Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        <ButtonSecond href="https://google.com">Ajuda</ButtonSecond>
+        <ButtonFirst href="https://google.com">Sair</ButtonFirst>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
---
+++ styled-components-abstract-your-css/src/Components/UI/variaveis.js
@@ -0,0 +1,3 @@
+export const colorFirst = "#41d3be";
+export const backgroundLight = "#f1f1f1";
+export const textBackgroundLight = "grey";
