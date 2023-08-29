24100e3388992e0f5b96349da045956c82df5ebd
Shigeru <proshigeru@gmail.com>
2023-08-29 16:03:22 -0300

    feat: created components extrato, item and itens

--- styled-components-abstract-your-css/src/Components/Extrato/index.jsx
+++ styled-components-abstract-your-css/src/Components/Extrato/index.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Box, Button } from "../UI/index";
 import { extratoLista } from "../../info";
+import Itens from "../Itens/Itens";
 
 const Extrato = () => {
   return (
     <Box>
       {extratoLista.updates.map((list) => {
-        return (
-          <div key={list.id}>
-            <div>{list.type}</div>
-            <div>{list.from}</div>
-          </div>
-        );
+        return <Itens key={list.id} list={list} />;
       })}
       <Button>Ver mais</Button>
     </Box>
