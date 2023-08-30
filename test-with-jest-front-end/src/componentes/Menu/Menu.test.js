6215cc77a7a00fd4d7f6dcd667fe1c27407cf96c
Shigeru <proshigeru@gmail.com>
2023-08-30 15:07:54 -0300

    test: testing links and extrato in menu

---
+++ test-with-jest-front-end/src/componentes/Menu/Menu.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import Menu from './index.jsx';
+
+const links = ['Inicial', 'Transferências', 'Investimentos', 'Outros serviços'];
+
+test('Should be render a link to start page', () => {
+  render(<Menu />);
+
+  links.map((link) => {
+    const linkStartPage = screen.getByText(link);
+    expect(linkStartPage).toBeInTheDocument();
+  });
+});
+
+test('Should be render a list link and return the amount desired', () => {
+  render(<Menu />);
+
+  const listLinks = screen.getAllByRole('link');
+  expect(listLinks).toHaveLength(4);
+});
+
+test('Should be not render the link to extrato', () => {
+  render(<Menu />);
+
+  const linkExtrato = screen.queryByText('Extrato');
+  expect(linkExtrato).not.toBeInTheDocument();
+});
