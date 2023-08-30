3d26dfae06d8cc5de6211611bbbfa3738316729f
Shigeru <proshigeru@gmail.com>
2023-08-30 10:21:45 -0300

    style: created themes light and dark

---
+++ styled-components-abstract-your-css/src/Components/UI/themes.js
@@ -0,0 +1,20 @@
+import {
+  backgroundLight,
+  textBackgroundLight,
+  contentLight,
+  backgroundBlack,
+  textBackgroundBlack,
+  contentBlack,
+} from "./variaveis";
+
+export const themeLight = {
+  body: backgroundLight,
+  inside: contentLight,
+  text: textBackgroundLight,
+};
+
+export const themeDark = {
+  body: backgroundBlack,
+  inside: contentBlack,
+  text: textBackgroundBlack,
+};
