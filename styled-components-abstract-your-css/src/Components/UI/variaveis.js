132c339b0ed9339b1b2e3b44d0250a44e0ad7222
Shigeru <proshigeru@gmail.com>
2023-08-30 10:21:06 -0300

    style: created variables theme dark

--- styled-components-abstract-your-css/src/Components/UI/variaveis.js
+++ styled-components-abstract-your-css/src/Components/UI/variaveis.js
@@ -1,3 +1,8 @@
 export const colorFirst = "#41d3be";
 export const backgroundLight = "#f1f1f1";
+export const contentLight = "white";
 export const textBackgroundLight = "grey";
+
+export const backgroundBlack = "#363537";
+export const contentBlack = "#5c5b5e";
+export const textBackgroundBlack = "#fafafa";
