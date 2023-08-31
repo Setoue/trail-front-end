fe3689736be481b3f0bfe3a9df79ee8d8fc3f589
Shigeru <proshigeru@gmail.com>
2023-08-31 15:34:56 -0300

    test: adding id test and testing if should render the balance with monetary value

---
+++ test-with-jest-front-end/src/componentes/Principal/Saldo/Saldo.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Saldo from './index';
+
+describe('component <saldo/>', () => {
+  test('should render the balance with monetary value', () => {
+    render(<Saldo saldo={1000} />);
+
+    const balance = screen.getByTestId('saldo');
+
+    expect(balance).toHaveTextContent('R$ 1000');
+  });
+});
